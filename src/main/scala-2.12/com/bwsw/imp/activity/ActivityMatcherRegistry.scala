676c348b31d2bad353b2ad94c3ddcbbf7f899433
Bitworks, Ltd <bitworks@bw-sw.com>
2017-08-05 16:35:47 +0700

    Implemented parallel action generation for events.

--- src/main/scala-2.12/com/bwsw/imp/activity/ActivityMatcherRegistry.scala
+++ src/main/scala-2.12/com/bwsw/imp/activity/ActivityMatcherRegistry.scala
@@ -1,8 +1,13 @@
 package com.bwsw.imp.activity
 
+import com.bwsw.imp.common.Lift
 import com.bwsw.imp.event.Event
+import org.slf4j.LoggerFactory
 
 import scala.collection.mutable
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, ExecutionContext, Future}
+import scala.util.{Failure, Success}
 
 /**
   * Created by Ivan Kudryavtsev on 01.08.17.
@@ -17,10 +22,28 @@ class ActivityMatcherRegistry(environment: Environment) {
   def spawn(event: Event): List[Activity] = spawnInt(registry.toList, event)
 
   private def spawnInt(registry: Seq[ActivityMatcher], event: Event): List[Activity] = {
-    registry match {
+    registry.toSeq match {
       case Nil => Nil
       case h :: t => h.spawn(environment, event) ++ spawnInt(t, event)
     }
-
   }
+
+  def spawnEvents(events: Seq[Event]): Seq[Activity] = {
+    import ExecutionContext.Implicits.global
+
+    val r = Lift.waitAll(events.map(e => Future { spawn(e) })) map {
+      res => res.flatMap(f => f match {
+        case Success(list) => list
+        case Failure(ex) => {
+          ActivityMatcherRegistry.logger.error("An exception occurred during Action generation for event.", ex)
+          Nil
+        }
+      })
+    }
+    Await.result(r, Duration.Inf)
+  }
+}
+
+object ActivityMatcherRegistry {
+  val logger = LoggerFactory.getLogger(this.getClass)
 }
---
+++ src/main/scala-2.12/com/bwsw/imp/common/Lift.scala
@@ -0,0 +1,19 @@
+package com.bwsw.imp.common
+
+import scala.concurrent._
+import scala.util.{Failure, Success}
+
+/**
+  * Created by ivan on 04.08.17.
+  */
+
+// based on
+// https://stackoverflow.com/questions/29344430/scala-waiting-for-sequence-of-futures
+//
+object Lift {
+  private def lift[T](futures: Seq[Future[T]]) =
+    futures.map(_.map { Success(_) }.recover { case t => Failure(t) })
+
+  def waitAll[T](futures: Seq[Future[T]]) =
+    Future.sequence(lift(futures))
+}
