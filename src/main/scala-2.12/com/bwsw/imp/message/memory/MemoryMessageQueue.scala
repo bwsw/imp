328588429b57c1c87420d089e57a4dc0d5e7cba8
bwsw <ivan.a.kudryavtsev@gmail.com>
2017-08-07 20:20:52 +0700

    Changed Message Queue implementation to linked blocking queue.

--- src/main/scala-2.12/com/bwsw/imp/message/memory/MemoryMessageQueue.scala
+++ src/main/scala-2.12/com/bwsw/imp/message/memory/MemoryMessageQueue.scala
@@ -1,39 +1,35 @@
 package com.bwsw.imp.message.memory
 
+import java.util.concurrent.LinkedBlockingQueue
+
 import com.bwsw.imp.message.{DelayedMessage, DelayedMessagesCpuProtection, Message, MessageQueue}
 
-import scala.collection.mutable
 
 /**
   * Created by Ivan Kudryavtsev on 06.08.17.
   */
+
 class MemoryMessageQueue extends MessageQueue with DelayedMessagesCpuProtection {
-  val queue = mutable.Queue[(Long, Message)]()
+  val queue = new LinkedBlockingQueue[(Long, Message)]()
 
   override def saveOffsets(): Unit = {}
 
   override def get: Seq[Message] = {
     delay()
-    if(queue.isEmpty) {
+    val elt = queue.poll()
+    if(getReadyTime < elt._1) {
+      queue.put(elt)
       incrementCpuProtectionDelay()
       Nil
-    }
-    else {
-      val elt = queue.dequeue()
-      if(getReadyTime < elt._1) {
-        queue.enqueue(elt)
-        incrementCpuProtectionDelay()
-        Nil
-      } else {
-        resetCpuProtectionDelay()
-        List(elt._2)
-      }
+    } else {
+      resetCpuProtectionDelay()
+      List(elt._2)
     }
   }
 
-  override def put(message: Message): Unit = queue.enqueue((0, message))
+  override def put(message: Message): Unit = queue.put((0, message))
 
-  override def putDelayed(message: DelayedMessage): Unit = queue.enqueue((message.delay, message))
+  override def putDelayed(message: DelayedMessage): Unit = queue.put((message.delay, message))
 
   override def loadOffsets(): Unit = {}
 }
