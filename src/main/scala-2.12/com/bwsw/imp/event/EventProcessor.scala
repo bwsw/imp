fee300ea9ee9fb4c291b24fcd6d5e4471b19d05d
Bitworks, Ltd <bitworks@bw-sw.com>
2017-08-07 17:34:35 +0700

    Improving logging for event processor. Added some debug output.

--- src/main/scala-2.12/com/bwsw/imp/event/EventProcessor.scala
+++ src/main/scala-2.12/com/bwsw/imp/event/EventProcessor.scala
@@ -4,6 +4,7 @@ import java.util.concurrent.atomic.AtomicBoolean
 
 import com.bwsw.imp.activity.{Activity, ActivityMatcherRegistry, DelayedActivity}
 import com.bwsw.imp.message.{MessageReader, MessageWritter}
+import org.slf4j.LoggerFactory
 
 /**
   * Created by Ivan Kudryavtsev on 05.08.17.
@@ -18,26 +19,49 @@ class EventProcessor(eventQueue: MessageReader,
       val messages = eventQueue.get
       val activities = estimator.filter(activityMatcherRegistry.spawnEvents(messages))
       activities.foreach {
-        case a: DelayedActivity => activityQueue.putDelayed(a)
-        case a: Activity => activityQueue.put(a)
+        case a: DelayedActivity =>
+          if(EventProcessor.logger.isDebugEnabled)
+            EventProcessor.logger.debug(s"Event processor put delayed activity $a to activity queue.")
+
+          activityQueue.putDelayed(a)
+        case a: Activity =>
+          if(EventProcessor.logger.isDebugEnabled)
+            EventProcessor.logger.debug(s"Event processor put regular activity $a to activity queue.")
+
+          activityQueue.put(a)
         case activity => throw new IllegalArgumentException(s"Unable to handle unknown activity type: $activity")
       }
+
+      if(EventProcessor.logger.isDebugEnabled)
+        EventProcessor.logger.debug(s"Event processor is going to save offsets.")
+
       eventQueue.saveOffsets
+
+      if(EventProcessor.logger.isDebugEnabled)
+        EventProcessor.logger.debug(s"Event processor have saved offsets.")
     }
   }
 
   var thread: Thread = _
   def start() = {
+    EventProcessor.logger.info("Event processor is going to start.")
     if(thread != null)
       throw new IllegalStateException("EventProcessor is already started.")
     thread = new Thread(() => { poll() })
     exit.set(false)
     thread.start()
+    EventProcessor.logger.info("Event processor is started.")
   }
 
   def stop() = {
+    EventProcessor.logger.info("Event processor is going to stop.")
     exit.set(true)
     thread.join()
     thread = null
+    EventProcessor.logger.info("Event processor is stopped.")
   }
+}
+
+object EventProcessor {
+  val logger = LoggerFactory.getLogger(this.getClass)
 }
