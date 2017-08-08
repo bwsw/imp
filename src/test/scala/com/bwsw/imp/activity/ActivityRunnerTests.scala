6d0fff55eb562dcd0eab56554c2111bc0165cbe1
Bitworks, Ltd <bitworks@bw-sw.com>
2017-08-08 12:18:06 +0700

    Implemented basic Activity Runner tests.

--- src/test/scala/com/bwsw/imp/activity/ActivityRunnerTests.scala
+++ src/test/scala/com/bwsw/imp/activity/ActivityRunnerTests.scala
@@ -1,5 +1,7 @@
 package com.bwsw.imp.activity
 
+import java.util.concurrent.{CountDownLatch, TimeUnit}
+
 import com.bwsw.imp.message.memory.MemoryMessageQueue
 import org.scalatest.{FlatSpec, Matchers}
 
@@ -11,6 +13,62 @@ class ActivityRunnerTests extends FlatSpec with Matchers {
     val runner = new ActivityRunner(regularActivityQueue = new MemoryMessageQueue,
       delayedActivityQueue = new MemoryMessageQueue, environment = new Environment)
     runner.start()
+    runner.stop()
+  }
+
+  it should "run single regular activity properly" in {
+    val regularActivityQueue = new MemoryMessageQueue
+    val latch = new CountDownLatch(1)
+    val runner = new ActivityRunner(regularActivityQueue = regularActivityQueue,
+      delayedActivityQueue = new MemoryMessageQueue, environment = new Environment)
+    runner.start()
+    regularActivityQueue.put(new Activity {
+      override def activate(e: Environment): Seq[Activity] = {
+        latch.countDown()
+        Nil
+      }
+    })
+    latch.await(10, TimeUnit.SECONDS) shouldBe true
+    runner.stop()
+  }
+
+  it should "run single regular activity which returns self for next run properly" in {
+    val regularActivityQueue = new MemoryMessageQueue
+    val latch = new CountDownLatch(2)
+    val runner = new ActivityRunner(regularActivityQueue = regularActivityQueue,
+      delayedActivityQueue = new MemoryMessageQueue, environment = new Environment)
+    runner.start()
+    regularActivityQueue.put(new Activity {
+      override def activate(e: Environment): Seq[Activity] = {
+        latch.countDown()
+        if(latch.getCount > 0)
+          Seq(this)
+        else
+          Nil
+      }
+    })
+    latch.await(10, TimeUnit.SECONDS) shouldBe true
+    runner.stop()
+  }
+
+  it should "run single regular activity which returns next for next run properly" in {
+    val regularActivityQueue = new MemoryMessageQueue
+    val latch = new CountDownLatch(2)
+    val runner = new ActivityRunner(regularActivityQueue = regularActivityQueue,
+      delayedActivityQueue = new MemoryMessageQueue, environment = new Environment)
+    runner.start()
+    regularActivityQueue.put(new Activity {
+      override def activate(e: Environment): Seq[Activity] = {
+        latch.countDown()
+        Seq(new Activity {
+          override def activate(e: Environment): Seq[Activity] = {
+            latch.countDown()
+            Nil
+          }
+        })
+      }
+    })
+    latch.await(10, TimeUnit.SECONDS) shouldBe true
     runner.stop()
   }
 }
