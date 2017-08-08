96333585d8f09bee18fdcffb54dd1495fa48fbcb
Bitworks, Ltd <bitworks@bw-sw.com>
2017-08-08 10:51:47 +0700

    Activity Runner WIP

--- src/main/scala-2.12/com/bwsw/imp/activity/ActivityRunner.scala
+++ src/main/scala-2.12/com/bwsw/imp/activity/ActivityRunner.scala
@@ -2,8 +2,13 @@ package com.bwsw.imp.activity
 
 import java.util.concurrent.CountDownLatch
 
-import com.bwsw.imp.common.StartStopBehaviour
-import com.bwsw.imp.message.MessageQueue
+import com.bwsw.imp.common.{Lift, StartStopBehaviour}
+import com.bwsw.imp.message.{MessageQueue, MessageReader}
+
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, ExecutionContext, Future}
+import scala.reflect.runtime.universe._
+import scala.util.{Failure, Success}
 
 /**
   * Created by Ivan Kudryavtsev on 07.08.17.
@@ -12,18 +17,38 @@ class ActivityRunner(regularActivityQueue: MessageQueue,
                      delayedActivityQueue: MessageQueue,
                      environment: Environment) extends StartStopBehaviour {
 
-  def pollRegularActivities() = {
+  private def pollRegularActivities() = {
     awaitStart.countDown()
     while(!isStopped) {
-      val activities = regularActivityQueue.get.map(_.asInstanceOf[Activity])
+      pollAndRun(regularActivityQueue, typeTag[Activity])
     }
   }
 
-  def pollDelayedActivities() = {
+  private def pollDelayedActivities() = {
     awaitStart.countDown()
     while(!isStopped) {
-      val delayedActivities = regularActivityQueue.get.map(_.asInstanceOf[DelayedActivity])
+      pollAndRun(delayedActivityQueue, typeTag[DelayedActivity])
     }
+  }
+
+  private def pollAndRun[A <: Activity](queue: MessageReader, cast: TypeTag[A]) = {
+    import ExecutionContext.Implicits.global
+
+    val activities = queue.get.map(_.asInstanceOf[A])
+    val activityFutures = Lift.waitAll(activities.map(activity => Future { activity.activateIt(environment) })) map {
+      res => res.flatMap(f => f match {
+        case Success(list) => list
+        case Failure(ex) =>
+          ActivityMatcherRegistry.logger.error("An exception occurred during running Activity.", ex)
+          Nil
+      })
+    }
+    Await.result(activityFutures, Duration.Inf) foreach {
+      case a: DelayedActivity => delayedActivityQueue.putDelayed(a)
+      case a: Activity => regularActivityQueue.put(a)
+      case a => throw new IllegalArgumentException(s"We expect only activity or delayed activity object, but received `$a'.")
+    }
+    queue.saveOffsets()
   }
 
   private var awaitStart: CountDownLatch = new CountDownLatch(2)
